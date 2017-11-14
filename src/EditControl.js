9a457a84e6cdc2eb853d05f2f0abde57371465ee
AaronFriel <mayreply@aaronfriel.com>
2017-11-13 18:05:09 -0600

    Added additional leaflet-draw handlers.

--- src/EditControl.js
+++ src/EditControl.js
@@ -7,7 +7,13 @@ import { Map } from 'leaflet';
 
 const eventHandlers = {
   onEdited: 'draw:edited',
+  onDrawStart: 'draw:drawstart',
+  onDrawStop: 'draw:drawstop',
+  onDrawVertex: 'draw:drawvertex',
   onEditStart: 'draw:editstart',
+  onEditMove: 'draw:editmove',
+  onEditResize: 'draw:editresize',
+  onEditVertex: 'draw:editvertex',
   onEditStop: 'draw:editstop',
   onDeleted: 'draw:deleted',
   onDeleteStart: 'draw:deletestart',

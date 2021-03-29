3fabfe9cdfc0a0a793822951bbd653dcbc678e8c
Serhii Khoma <srghma@gmail.com>
2021-03-29 20:17:54 +0300

    feat: merge 59 -> Use the 'fast-deep-equal'

--- src/EditControl.js
+++ src/EditControl.js
@@ -1,6 +1,6 @@
 import { PropTypes } from 'prop-types';
 import Draw from 'leaflet-draw'; // eslint-disable-line
-import isEqual from 'lodash-es/isEqual';
+import isEqual from 'fast-deep-equal';
 import React, { useRef } from 'react';
 import { useLeafletContext } from '@react-leaflet/core';
 
